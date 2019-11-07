043687a845440bfc2b1fb0541af21cea25207747
SimonStill <57300255+SimonStill@users.noreply.github.com>
2019-11-07 18:34:56 -0500

    Add files via upload

---
+++ R02_XP00.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+ *  Rule02 - XP00
+ *
+ ******************************************************************************/
+
+
+public static void deleteFile (){
+    File someFile = new File ("someFileName.txt "); 
+    // Do something with someFile
+    someFile.delete ();
+}
+    
+
